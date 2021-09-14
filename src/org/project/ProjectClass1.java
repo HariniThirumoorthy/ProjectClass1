bf2b5a0b13e373f78d015a0785bf2c710d085d07
HariniThirumoorthy <harinee1995@gmail.com>
2021-09-14 09:16:38 +0530

    Completed

---
+++ src/org/project/ProjectClass1.java
@@ -0,0 +1,13 @@
+package org.project;
+
+public class ProjectClass1 {
+	
+	private void add(int a, int b) {
+int c= a+b;
+System.out.println(c);
+	}
+public static void main(String[] args) {
+	ProjectClass1 c = new ProjectClass1();
+	c.add(5, 5);
+}
+}
